2fcedf64688a3dabc5937142292cfc89a1fdc571
vedpatel1010 <vedsanketbhaipatel@gmail.com>
2025-08-11 19:53:45 +0530

    Update Rockpapersiccorgame.py

--- my_projects/Rockpapersiccorgame.py
+++ my_projects/Rockpapersiccorgame.py
@@ -1,16 +1,38 @@
-item = str(input("what whould you like to buy today? \nHere is the list: Ammonia , HCL , Nitric Acid:"))
-if item == "Ammonia":
-    print("The price of ammonia is 40 rupess per litre")
-    quantity1 = float(input("How many litres you need? "))
-    total1 = quantity1 * 40
-    print(f"Your total is {total1}")
-elif item == "HCL":
-    print("The price of HCL is 60 rupess per litre")
-    quantity2 = float(input("How many litres you need? "))
-    total2 = quantity2 * 60
-    print(f"Your total is {total2}")
-elif item == "Nitric Acid":
-    print("The price of Nitric Acid is 70 rupess per litre")
-    quantity3 = float(input("How many litres you need? "))
-    total3 = quantity3 * 70
-    print(f"Your total is {total3}")
+import random
+p = 0
+c = 0
+for a in range(3):
+    x = random.randint(1, 3)
+    y = int(input("Enter what have you chosen:\n 1) Rock \n 2) Paper \n 3) scissor \n : "))
+    while y > 3:
+        print("invalid option chossen")
+        y = int(input("Enter what have you chosen:\n 1) Rock \n 2) Paper \n 3) scissor \n : "))
+
+    if x == y :
+        print("Both chosen same")
+    elif x == 2 and y == 3:
+        print("Player Wins")
+        p = p + 1
+    elif x ==2 and y == 1:
+        print("Computer Wins")
+        c  = c + 1
+    elif x == 1 and y == 3:
+        print("Computer Wins")
+        c = c + 1
+    elif x == 1 and y == 2:
+        print("Player Wins")
+        p = p + 1
+    elif x == 3 and y == 1:
+        print("Player Wins")
+        p = p + 1
+    elif x == 3 and y == 2:
+        print("Computer Wins")
+        c = c + 1
+    print(f"The computer choose {x}")
+print(f"The total score is Computer:{c} and Player:{p}")
+if c > p:
+    print("The Computer Wins!")
+elif c == p:
+    print("The scores were same it's a Tie!")
+else :
+    print("The Player Wins!")
